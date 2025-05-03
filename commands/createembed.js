0fd22eec2f2222a7e51ed0d41b0b75be936d8255
kaipluxury <kyatutheekhai@gmail.com>
2025-05-03 19:14:21 +0000

    Update createembed.js

--- commands/createembed.js
+++ commands/createembed.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,42 +16,22 @@ module.exports = {
       .setCustomId("embedModal")
       .setTitle("Create Custom Embed");
 
-    const input1 = new TextInputBuilder()
-      .setCustomId("embedTitle")
-      .setLabel("Title")
-      .setStyle(TextInputStyle.Short)
-      .setRequired(true);
+    const inputs = [
+      ["embedTitle", "Title", TextInputStyle.Short],
+      ["embedDesc", "Description", TextInputStyle.Paragraph],
+      ["embedImage", "Image URL (optional)", TextInputStyle.Short],
+      ["embedThumb", "Thumbnail URL (optional)", TextInputStyle.Short],
+      ["embedFooter", "Footer Text (optional)", TextInputStyle.Short]
+    ].map(([id, label, style]) =>
+      new ActionRowBuilder().addComponents(
+        new TextInputBuilder()
+          .setCustomId(id)
+          .setLabel(label)
+          .setStyle(style)
+          .setRequired(false)
+      )
+    );
 
-    const input2 = new TextInputBuilder()
-      .setCustomId("embedDesc")
-      .setLabel("Description")
-      .setStyle(TextInputStyle.Paragraph)
-      .setRequired(true);
-
-    const input3 = new TextInputBuilder()
-      .setCustomId("embedImage")
-      .setLabel("Image URL (optional)")
-      .setStyle(TextInputStyle.Short)
-      .setRequired(false);
-
-    const input4 = new TextInputBuilder()
-      .setCustomId("embedThumb")
-      .setLabel("Thumbnail URL (optional)")
-      .setStyle(TextInputStyle.Short)
-      .setRequired(false);
-
-    const input5 = new TextInputBuilder()
-      .setCustomId("embedFooter")
-      .setLabel("Footer Text (optional)")
-      .setStyle(TextInputStyle.Short)
-      .setRequired(false);
-
-    const row1 = new ActionRowBuilder().addComponents(input1);
-    const row2 = new ActionRowBuilder().addComponents(input2);
-    const row3 = new ActionRowBuilder().addComponents(input3);
-    const row4 = new ActionRowBuilder().addComponents(input4);
-    const row5 = new ActionRowBuilder().addComponents(input5);
-
-    await interaction.showModal(modal.addComponents(row1, row2, row3, row4, row5));
+    await interaction.showModal(modal.addComponents(...inputs));
   }
 };
